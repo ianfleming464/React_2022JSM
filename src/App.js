3a8336f68b4c67dd8b65d3e73270dfb5639c0358
ianfleming464 <60020259+ianfleming464@users.noreply.github.com>
2022-03-07 21:35:38 +0100

    Added dynamic info form API, hooks to display.
    Fin!

--- src/App.js
+++ src/App.js
@@ -1,55 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import MovieCard from './MovieCard';
 import './App.css';
 import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=637033dc';
 
-const movie1 = {
-  Title: 'Terminator 2: Judgment Day',
-  Year: '1991',
-  imdbID: 'tt0103064',
-  Type: 'movie',
-  Poster:
-    'https://m.media-amazon.com/images/M/MV5BMGU2NzRmZjUtOGUxYS00ZjdjLWEwZWItY2NlM2JhNjkxNTFmXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
-};
-
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchMovies('Batman');
+  }, []);
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data);
+    setMovies(data.Search);
   };
-  useEffect(() => {
-    searchMovies('Terminator');
-  }, []);
 
   return (
     <div className='app'>
-      <h1>Movieland</h1>
+      <h1>MovieLand</h1>
+
       <div className='search'>
-        <input placeholder='Search for a movie!' value='Superman' onChange={() => {}}></input>
-        <img src={SearchIcon} alt='search' onClick={() => {}} />
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder='Search for movies'
+        />
+        <img src={SearchIcon} alt='search' onClick={() => searchMovies(searchTerm)} />
       </div>
-      <div className='container'>
-        <div className='movie'>
-          <div>
-            <p>{movie1.Year}</p>
-          </div>
 
-          <div>
-            <img
-              src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'}
-              alt={movie1.Title}
-            />
-          </div>
-
-          <div>
-            <span>{movie1.Type}</span>
-            <h3>{movie1.Title}</h3>
-          </div>
+      {movies?.length > 0 ? (
+        <div className='container'>
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
         </div>
-      </div>
+      ) : (
+        <div className='empty'>
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 };
